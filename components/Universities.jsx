ddc64ec990ed3d587e470f1dcb9f0093bcf1fe4a
Rahmat Ali <sk.rahmatali6792@gmail.com>
2025-08-30 23:01:48 +0600

    remove usused code

--- components/Universities.jsx
+++ components/Universities.jsx
@@ -1,46 +1,8 @@
 import React from "react";
-import brown from "../public/assets/img/partner/brown.svg";
-import yale from "../public/assets/img/partner/yale.svg";
-import dartmouth from "../public/assets/img/partner/dartmouth.svg";
-import columbia from "../public/assets/img/partner/columbia.svg";
-import duke from "../public/assets/img/partner/duke.svg";
-import berkeley from "../public/assets/img/partner/berkeley.svg";
-import michigan from "../public/assets/img/partner/michigan.svg";
 import Marquee from "react-fast-marquee";
 import Image from "next/image";
 
 export default function Universities() {
-  const partners = [
-    {
-      logo: brown,
-      className: "w-[103px] md:w-[135px]",
-    },
-    {
-      logo: yale,
-      className: "w-[57px] md:w-[75px]",
-    },
-    {
-      logo: dartmouth,
-      className: "w-[42px] md:w-[55px]",
-    },
-    {
-      logo: columbia,
-      className: "w-[100px] md:w-[148px]",
-    },
-    {
-      logo: duke,
-      className: "w-[80px] md:w-[117px]",
-    },
-    {
-      logo: berkeley,
-      className: "w-[96px] md:w-[140px]",
-    },
-    {
-      logo: michigan,
-      className: "w-[176px] md:w-[246px]",
-    },
-  ];
-
   const universityLogos = [
     {
       logo: "berkeley.edu.jpg",
